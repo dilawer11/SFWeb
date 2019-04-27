0cb9563c27b11b6397e61eceb1b11586b06abeb7
Aleena Azam <aleena.azam796@gmail.com>
2019-04-27 17:35:37 +0500

    Update Order and Query

--- src/components/Client/Order.vue
+++ src/components/Client/Order.vue
@@ -1,76 +1,484 @@
 <template>
-  <div id="order">
-      <p v-if="feedback">{{feedback}}</p>
-    <section class="testimonials padding-top-90 padding-bottom-90" id="testimonials">
-      <div class="container">
-        <h1>Order</h1>
-      </div>
-    </section>
-    <!--Start Portfolio Section-->
-    <section class="portfolio padding-top-90 padding-bottom-60 primary_bg" id="portfolio">
-      <div class="container">
-        <div class="sec_title">
-          <p>Order</p>
-          <h2>Form</h2>
-        </div>
-        <table class="table">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Product Name</th>
-              <th scope="col">Size</th>
-              <th scope="col">Quantity</th>
-              <th scope="col">Price</th>
-              <th scope="col">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item,index) in orderItems" :key="index">
-              <th scope="row">{{index+1}}</th>
-              <td>{{item.name}}</td>
-              <td>{{item.size}}</td>
-              <td>{{item.qty}}</td>
-              <td>{{item.price}}</td>
-              <td>{{item.total}}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+   <div id="order">
+        <section class="testimonials padding-top-90 padding-bottom-90" id="testimonials">         
+            <div class="center-align container">
+                <h1>Order Form</h1>
+            </div>
+        </section>
 
-      <!--.container-->
+
+    <section class="portfolio" id="portfolio">
+    
+        <div class="container">
+            <div class="formBox">
+
+                <form>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h3 >First Name: </h3>
+                            <div class="inputBox ">
+                                <input ref="FirstName" v-model=FirstName type="text" class="input" >
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <h3>Last Name: </h3>
+                            <div class="inputBox ">
+                                <input ref="LastName" v-model=LastName type="text" class="input" >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h3>Email: </h3>
+                            <div class="inputBox ">
+                                <input v-model=Email type="text" class="input" >
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <h3>Phone: </h3>
+                            <div class="inputBox ">
+                                <input v-model=Phone type="text" class="input" @keydown="isPhoneNumber">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="row">
+                        <form class="add-item padding-bottom-2 padding-right-2  padding-left-2 padding-top-2 bg-info" @submit.prevent="AddProduct">
+
+                            <h3>Add product:</h3>
+                            
+                            <div class="row">
+
+                                <div class="form-group">
+                                    <label class="control-label col-md-2" for="item" style=" font-size: 16px;">Item Name</label>
+                                        <div class="col-md-4">
+                                            <select id="item-name" class="form-control" v-model=currentProduct>
+                                                <option :value=p v-for="p in products" :key="p.id" >{{p.name}}</option>                   
+                                            </select> 
+                                        </div>
+
+                                    <label class="control-label col-md-2" for="quantity" style=" font-size: 16px;">Quantity</label>
+                                        <div class="col-md-4">
+                                            <input type="text" id="quantity" class="form-control" style="width: 100%" v-if="!(this.currentProduct===null)" placeholder=" " v-model=selected.quantity  @keypress="isNumber"></input> 
+                                            <input type="text" id="quantity" class="form-control" style="width: 100%" v-else disabled placeholder="Chose an item first" v-model=selected.quantity @keypress="isNumber" @keyup="isNumber"></input> 
+                                        </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <label class="control-label col-md-2" for="size" style=" font-size: 16px;">Size</label>
+                                        <div class="col-md-4">
+                                            <select id="size" class="form-control" v-model=selected.sizes >
+                                                <option :value="sizes" v-for="(sizes,index) in currentSizePrice" :key="index">{{sizes.size}}</option> 
+                                            </select> 
+                                            
+                                        </div>
+                                    <label class="control-label col-md-2" for="price" style=" font-size: 16px;">Price</label>
+                                        <div class="col-md-4">
+                                            <label id="price" class="form-control" >{{selected.sizes.price}}</label>
+                                        </div>
+                                </div>
+                            </div>
+              
+                            <div class="row right-text">
+                                <div class="col-sm-10">
+
+                                    <label v-if="!Total" style="font-size: 16px;" id="price">Total: 0</label>
+                                    <label v-if="Total" style="font-size: 16px;" id="price">Total: {{Total}}</label>
+                                </div>
+                                <div class="col-sm-2">
+                                    <input type="submit" class="btn btn-primary " value="Add to Cart"></input>
+                                </div>
+                                
+                                
+                               
+                                
+                            </div>
+                        </form>
+                    </div>
+
+
+                    <label v-if="this.feedback" style="color:rgba(151, 26, 26, 0.5);">{{feedback}}</label>
+                    <label v-if="this.feedback===null"> </label>
+
+                    <div class="table-responsive padding-top-2">
+                        <table class="table" v-if="this.Cart.length!==0">
+                            <thead>
+                                <tr>
+                                    <th>Item Name</th>
+                                    <th>Quantity</th>
+                                    <th>Size</th>
+                                    <th>Price</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                <tr v-for="(item, index) in this.Cart" :key="index">
+                                
+                                    <th>{{item.name}}</th>
+                                    <th>{{item.quantity}}</th>
+                                    <th>{{item.sizes.size}}</th>
+                                    <th>{{item.sizes.price}}</th>
+                                    <th>{{item.total}}</th>
+
+                                </tr>
+
+
+                            </tbody>
+
+                            <tfoot>
+                                <tr>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th> </th>
+                                    <th>{{OrderTotal}}</th>
+                                </tr>
+                            </tfoot>
+
+                        </table>
+                    </div>
+
+
+                    <div class="row padding-top-90">
+                        <div class="btn-toolbar pull-right">
+                            <button type="button" id="placeOrder" class="btn btn-primary btn-lg" @click="PlaceOrder">Place Order</button>
+                            <button type="button" id="getQuote" class="btn btn-primary btn-lg">Get Price Quote</button>
+                        </div>
+                    </div>
+                </form>
+
+
+
+
+            </div>
+        </div>
+
+        
     </section>
-    <!--End Portfolio Section-->
-  </div>
+                     
+
+    
+    </div>
 </template>
 
 <script>
+
+import db from '@/firebase/init'
+import { constants } from 'crypto';
+import { COPYFILE_EXCL } from 'constants';
+
 export default {
-  name: "Order",
-  data() {
-    return {
-        feedback:null,
-      orderItems: [
-        {
-          name: "Cotton Wool",
-          size: "500gm",
-          qty: 3,
-          price: 50,
-          total: 150
-        },
-        {
-          name: "Cotton Bandage",
-          size: "3in x 5mtr",
-          qty: 4,
-          price: 20,
-          total: 80
+    name: 'Order',
+    data(){
+        return{
+            products: [],
+            currentProduct: null,
+            currentSizePrice: null,
+
+            ITEM:{
+                name:null,
+                price: 0,
+                quantity:0,
+                size: null,
+                total:0
+            },
+
+
+            selected:{
+                name:null,
+                quantity:null,
+                sizes:{
+                    price: null,
+                    size: null
+                },
+                total:0
+            },
+            FirstName:null,
+            LastName:null,
+            Email: "",
+            Phone: "",
+
+            Cart: [],
+            feedback:null
+
         }
-      ],
-      another: null,
-      orderTotalPrice: 230
-    };
-  }
-};
+    },
+    methods:{
+
+            isPhoneNumber(event)
+            {
+                var phoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+                if(inputtxt.value.match(phoneno))
+                {
+                    return true;      
+                }
+                else
+                {
+                    this.feedback="Not a valid Phone Number";
+                    return false;
+                }
+            },
+            isPhoneNumber(event){
+            if (!(event.key === "Backspace" || event.key === "Delete" || event.key==="+"))
+            {
+                if (!(event.key === 'Enter' || (Number(event.key) >= 0 && Number(event.key) <= 9)))
+                {
+                    this.feedback="Phone Number can only contain numbers";
+                    event.preventDefault();
+                }
+                else if(this.Phone.length>=13)
+                {
+                    this.feedback="Phone Number can only contain 13 numbers";
+                    event.preventDefault();
+
+                }
+                else
+                {
+                    this.feedback=null
+                }
+            }
+            else
+            {
+                 this.feedback=null
+            }
+           
+        },
+        isNumber(event){
+            if (!(event.key === 'Enter' || (Number(event.key) >= 0 && Number(event.key) <= 9)))
+            {
+                this.feedback="Quantity must be a number";
+                event.preventDefault();
+            }
+            else
+            {
+                this.feedback=null
+            }
+
+        },
+           
+        AddProduct(e){
+            if(isNaN(this.selected.quantity))
+            {
+                this.feedback="QUANTITY MUST BE A NUMBER"
+                this.selected.quantity=null;
+            }
+            if(this.selected.name!==null && this.selected.quantity!==null && this.selected.sizes!=={} && this.selected.price!==0)
+            {
+                var temp = this.selected.quantity * this.selected.sizes.price
+                
+                this.selected.total=temp
+                console.log("hereee")
+                console.log(this.selected.total)
+                var temp=Object.assign({},this.selected)
+                this.Cart.push(temp)
+            
+                this.$nextTick(() => {
+            	this.currentProduct = null
+                })
+            }
+            else
+            {
+                this.feedback="Incomplete entries in the form"
+            }
+            
+
+         
+        },
+        PlaceOrder(e){
+            var temp="";
+            if(!this.LastName||!this.FirstName || !this.Phone || !this.Email)
+            {
+                temp+="Missing details in the form \n"
+            }
+
+            
+            let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (this.Email && !(re.test(String(this.Email).toLowerCase()) && this.Email.length>4))
+            {
+                temp+="Enter a Valid Email \n"
+                
+            }
+          
+            if(this.Cart.length===0){
+                temp+=" Add products to Cart before placing order\n"
+            }
+
+            if(temp!=="")
+            {
+                this.feedback=temp;
+            }
+            else{
+                this.feedback=null
+                var Fullname=this.FirstName+' '+ this.LastName
+                
+                // var orderTotal=0;
+                // let i=0
+                // for(i=0; i<this.Cart.length;  i=i+1)
+                // {
+                //     t+=this.Cart[i].total
+
+                // }
+                
+                console.log(Fullname)                
+                console.log(this.Email)
+                console.log(this.Phone)
+                console.log(this.Cart)
+
+                db.collection('products').add({
+                    email:this.Email,
+                    items:this.Cart,
+                    name:this.Fullname,
+                    no:0,
+                    phone:this.Phone,
+                    status:"SENT",
+                    total:0
+                 
+
+                }).then(()=>{
+                    this.loading=false;
+                    alert('Order Sucessfully Place')
+                    this.$router.push({name:'Dashboard'})
+                }).catch(err=>{
+                    this.loading=false;
+                    alert('Something went wrong please try again later')
+                    this.$router.push({name:'Dashboard'})
+                })
+                
+// email "dilawer11@gmail.com"
+// id "MkQfASi57iF2uq69HjjQ"
+// items [{name: "Cotton Wool", pri...]
+// name "Dilawer"
+// no 21
+// phone "+923234443331"
+// status "SENT"
+// total 190 
+
+
+            }
+
+        }
+
+    },
+    computed: {
+        OrderTotal()
+        {
+            var t=0;
+            var i=0
+            for(i=0; i<this.Cart.length;  i=i+1)
+            {
+                t+=this.Cart[i].total
+
+            }
+            return t;
+            
+        },
+        Total()
+        {
+            return this.selected.quantity * this.selected.sizes.price
+        }
+    },
+    watch: {
+        selected: function (val)
+        {
+            if(val.sizes.size===null)
+            {
+                val.sizes.price=null
+            }
+        },
+            Email: function (val){
+                let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (val && (re.test(String(val).toLowerCase()) && val.length>4))
+                {
+                    this.feedback=null;
+                }
+            },
+            Phone: function(val)
+            {
+                if (val.length===11)
+                {
+                    this.feedback=null;
+                }
+            },
+            currentProduct: function (val) {
+
+            if(val!==null)
+            {
+                this.selected.name=val.name
+                this.currentSizePrice = val.sizes
+            }
+            else
+            {
+                this.currentProduct= null
+                this.currentSizePrice= null
+                this.selected.name=null
+                this.selected.quantity=null
+                this.selected.sizes={}
+                this.selected.total=0;
+               
+            }
+        }
+        
+    },    
+    created()
+    {
+        //fetch data from firestore
+        db.collection('products').get()//returns a promise
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                let p = doc.data()
+                p.id = doc.id
+                this.products.push(p)
+            });
+    })//then is fired when promise completes
+
+
+
+
+    }
+}
 </script>
 
+
 <style>
+.input-group-addon {
+    min-width:8em;
+    text-align:left;
+}
+.padding-bottom-50{
+
+    padding-bottom:5em;
+    
+}
+.padding-top-2{
+
+    padding-top:2em;
+    
+}
+.padding-bottom-2{
+
+    padding-bottom:2em;
+    
+}
+.padding-left-2{
+
+    padding-left:2em;
+    
+}
+.padding-right-2{
+
+    padding-right:2em;
+    
+}
+.highlight{
+    color: darkgray
+}
 </style>
--- src/components/Client/Query.vue
+++ src/components/Client/Query.vue
@@ -1,113 +1,201 @@
 <template>
-  <div id="query">
-    <section class="testimonials padding-top-90 padding-bottom-90" id="testimonials">
-      <div class="container">
-        <h1>Contact Us</h1>
-      </div>
+    <div id="query">
+        <section class="testimonials padding-top-90 padding-bottom-90" id="testimonials">         
+            <div class="container">
+                <h1>Contact Us</h1>
+            </div>
+        </section>
+         <!--Start Portfolio Section-->
+    <section class="portfolio" id="portfolio">
+    
+        <div class="container">
+            <div class="formBox">
+                <form>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h1>Query Form</h1>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            
+                            <h2>First Name: </h2>
+                            <div class="inputBox ">
+                                <input v-model=firstname type="text" class="input" >
+                            </div>
+                            
+                        </div>
+
+                        <div class="col-sm-6">
+                            <h2>Last Name: </h2>
+                            <div class="inputBox ">
+                                <input v-model=lastname type="text" class="input" >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h2>Email: </h2>
+                            <div class="inputBox ">
+                                <input v-model=email type="text" class="input" >
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6">
+                            <h2>Phone: </h2>
+                            <div class="inputBox ">
+                                <input v-model=phone type="text" class="input" >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2>Message: </h2>
+                            <div class="inputBox">
+                                <textarea v-model=message class="input"></textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-sm-12">
+                            <input type="submit" name="Send" class="button btn-default btn-lg " @click="sendQuery">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </section>
-    <!--Start Portfolio Section-->
-    <section class="portfolio padding-top-90 padding-bottom-60 primary_bg" id="portfolio">
-      <h2 class="center-align black-text">Query Form</h2>
+                     
 
-      <div class="container">
-        <div class="divider"></div>
-        <br>
-        <br>
-        <br>
-        <br>
-        <form @submit.prevent class="col s12">
-          <div class="row">
-            <div class="input-field col s12 md6">
-              <i class="material-icons prefix">account_circle</i>
-              <input v-model="firstname" id="first_name" type="text">
-              <label class="active grey-text" for="first_name">First Name</label>
-            </div>
-            <div class="input-field col s12 m6">
-              <i class="material-icons prefix">account_circle</i>
-              <input v-model="lastname" id="last_name" type="text">
-              <label class="active grey-text" for="last_name">Last Name</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="input-field col s12 m6">
-              <i class="material-icons prefix">email</i>
-              <input v-model="email" id="email" type="email">
-              <label class="active grey-text" for="email">Email</label>
-            </div>
-            <div class="input-field col s12 m6">
-              <i class="material-icons prefix">phone</i>
-              <input v-model="phone" id="phone" type="tel" placeholder="+12-123-1234567">
-              <label class="active grey-text" for="phone">Phone</label>
-            </div>
-          </div>
 
-          <div class="row">
+
+    <!-- <div class="divider"></div>
+    <br><br>
+    <br><br>
+    <form class="col s12">
+        <div class="row">
+            <div class="input-field col s12 m6">
+                <i class="material-icons prefix">account_circle</i>
+                <input v-model="firstname" id="first_name" type="text">
+                <label class="active grey-text" for="first_name">First Name</label>
+            </div>
+            <div class="input-field col s12 m6">
+                <i class="material-icons prefix">account_circle</i>
+                <input v-model="lastname" id="last_name" type="text" >
+                <label class="active grey-text" for="last_name">Last Name</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12 m6">
+                <i class="material-icons prefix">email</i>
+                <input v-model="email" id="email" type="email">
+                <label class="active grey-text" for="email">Email </label>
+            </div>
+            <div class="input-field col s12 m6">
+                <i class="material-icons prefix">phone</i>
+                <input v-model="phone" id="phone" type="tel" placeholder="+12-123-1234567">
+                <label class="active grey-text" for="phone">Phone</label>
+            </div>
+        </div>
+  
+        <div class="row">
             <div class="input-field col s12 m12">
-              <i class="material-icons prefix">mode_edit</i>
-              <textarea
-                v-model="message"
-                id="message"
-                class="materialize-textarea grey-text"
-                data-length="250"
-              ></textarea>
-              <label class="active grey-text" for="message">Message</label>
+            <i class="material-icons prefix">mode_edit</i>
+            <textarea  v-model="message" id="message" class="materialize-textarea grey-text" data-length=250></textarea>
+            <label class="active grey-text" for="message">Message</label>
             </div>
-          </div>
-          <button @click="verifyQuery" class="btn waves-effect waves-light grey">
+        </div>
+        <button @click="sendQuery" class="btn waves-effect waves-light grey">
             Send
             <i class="material-icons right">send</i>
-          </button>
-        </form>
-      </div>
-    </section>
-    <!--End Portfolio Section-->
-  </div>
+        </button> 
+    </form> -->
+
+                <!--End Portfolio Section-->
+    </div>
 </template>
 
 <script>
-// import userConfig from "@/firebase/userConfig.js";
-import db from "@/firebase/init";
-import firebase from 'firebase'
-export default {
-  name: "Query",
-  data() {
-    return {
-      firstname: null,
-      lastname: null,
-      email: null,
-      phone: null,
-      message: null,
-      feedback: null
-    };
-  },
-  methods: {
-    verifyQuery() {
-      //TODO: Add Data Validation here
-      this.sendQuery();
-    },
-    sendQuery() {
 
-        //Send through email
-        let emailBody = 'Name : ' + this.firstname + ' ' + this.lastname + '\n\n' + 'Email: ' + this.email + '   Phone: ' + this.phone + '\n\n' + this.message
-        var sendMail = firebase.functions().httpsCallable('sendMail');
-        sendMail({body: emailBody, to: 'dilawer11@gmail.com', from: 'Surgical Fibre <noreply-surgicalfibre@surgicalfibre.com>'}).then(result=>{
-            alert('Your Query Has Been Recieved')
-            console.log(result.result)
-        }).catch(err=>{
-            alert('Error: Your Query Was Not Sent')
-            console.log(err)
-        })
-        
+export default {
+    name: 'Query',
+    data(){
+        return{
+            firstname: null,
+            lastname: null,
+            email: null,
+            phone: null,
+            message: null
+        }
+    },
+    methods:{
+        sendQuery(){
+            alert('Your Query Has Been Sent')
+            this.$router.push({name:'Query'})
+            console.log(this.firstname,this.lastname)
+            console.log(this.email,this.phone)
+            console.log(this.message)
+        }
     }
-  }
-};
+}
 </script>
 
 <style>
-.query {
-  margin-top: 50px;
+.query{
+    margin-top: 50px;
 }
-textarea.materialize-textarea {
-  height: 10rem;
+body{
+	font-family: sans-serif;
+}
+.formBox{
+	margin-top: 45px;
+    margin-bottom: 80px;
+}
+.formBox  h1{
+	text-align: center;
+	margin-bottom: 50px;
+	text-transform: uppercase;
+	font-size: 40px;
+}
+.formBox  h2{
+	font-size: 25px;
+}
+.inputBox{
+	position: relative;
+	box-sizing: border-box;
+	margin-bottom: 50px;
+}
+.inputBox .inputText{
+	position: absolute;
+    font-size: 24px;
+    line-height: 50px;
+    transition: .5s;
+    opacity: .5;
+}
+.inputBox .input{
+	position: relative;
+	width: 100%;
+	height: 50px;
+	background: transparent;
+	border: none;
+    outline: none;
+    font-size: 15px;
+    border-bottom: 1px solid rgba(0,0,0,.5);
+}
+.input:focus{
+    border-bottom: 3px solid rgba(5, 79, 128, 0.5);
+
+}
+textarea{
+	height: 100px !important;
+}
+.button{  
+    background:rgba(3, 53, 94, 0.5);
+    color: #fff;
 }
 </style>
