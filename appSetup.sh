bc69b3951f7114869d8f5d29813a45c1dc126c06
Dilawer Ahmed <dilawer11@gmail.com>
2019-05-17 08:57:20 +0500

    update appsetup

--- appSetup.sh
+++ appSetup.sh
@@ -1,10 +1,13 @@
 if [ "$1"  == "mac" ]; then
 	brew install node
 	brew install npm
+	brew install git
 elif [ "$1" == "linux" ]; then
 	sudo apt-get install nodejs npm
+	sudo apt-get install git
 else 
 	echo "Infering you have npm and nodejs installed and running the later commands"
 fi
+git clone https://github.com/dilawer11/SFWeb.git
 npm install
 npm run serve
